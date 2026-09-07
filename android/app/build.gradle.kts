a023906399c9c65b03d0cc406f86601615e1e9f1
muadkhalidalsadiq-maker <muadkhalidalsadiq@users.noreply.github.com>
2026-09-07 08:42:27 +0200

    🔄 Change Bundle ID to com.muadalsadiq.taybah & enable core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,34 +5,27 @@ plugins {
 }
 
 android {
-    namespace = "com.example.taybah"
+    namespace = "com.muadalsadiq.taybah"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.taybah"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.muadalsadiq.taybah"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the `-P force-version-code-ignoring-abi=true`
-        // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -42,6 +35,10 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
--- android/app/src/main/kotlin/com/muadalsadiq/taybah/MainActivity.kt
+++ android/app/src/main/kotlin/com/muadalsadiq/taybah/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.taybah
+package com.muadalsadiq.taybah
 
 import io.flutter.embedding.android.FlutterActivity
 
